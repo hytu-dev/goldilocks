cefe0664c66c8382faa12a8fc2410f65f2f220b9
Hung-Yuan Tu <release@hytu.dev>
2026-08-07 12:34:28 +0800

    feat: implement <goldi-break> web component

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export {};
+export { GoldiBreak } from "./component.ts";
+export * from "./defs.ts";
+export { hyphenate } from "./hyphenate/index.ts";
+export { measure } from "./measure/index.ts";
+export { nodify } from "./nodify/index.ts";
+export { render } from "./render/index.ts";
+export { type Break, type SolveOptions, solve } from "./solve/index.ts";
